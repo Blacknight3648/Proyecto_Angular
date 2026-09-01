d236788363fe33465c388a61758be1ff877bc544
Cristopher Osses <cr.ossesg@duocuc.cl>
2026-09-01 01:24:29 -0400

    Update frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,28 @@
 import { Component, signal } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [RouterOutlet, NgIf]
 })
 export class App {
   protected readonly title = signal('frontend-simple');
+
+  constructor(private msal: MsalService) {}
+
+  isLoggedIn(): boolean {
+    return this.msal.instance.getAllAccounts().length > 0;
+  }
+
+  login(): void {
+    this.msal.loginRedirect();
+  }
+
+  logout(): void {
+    this.msal.logoutRedirect();
+  }
 }
